cba7bf171b2ac2d31e00e027cc2270e77462a71f
Bhaskar-Developer <koderulzstack@gmail.com>
2023-04-24 13:57:37 +0530

    Add cors

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const colors = require('colors')
+const cors = require('cors')
 require('dotenv').config()
 const { graphqlHTTP } = require('express-graphql')
 const schema = require('./schema/schema')
@@ -7,6 +8,9 @@ const connectDB = require("./config/db")
 const port = process.env.PORT || 5000
 
 const app = express()
+
+// use CORS
+app.use(cors())
 
 // Connect to DB
 connectDB()
