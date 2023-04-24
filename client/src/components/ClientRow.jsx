ecca697e619cfe278ea3bf9527c2d138cde8b502
Bhaskar-Developer <koderulzstack@gmail.com>
2023-04-24 16:12:41 +0530

    Delete client projects when client deleted

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -2,20 +2,12 @@ import { FaTrash } from 'react-icons/fa';
 import { useMutation } from '@apollo/client';
 import { DELETE_CLIENT } from '../mutations/clientMutations';
 import { GET_CLIENTS } from '../queries/clientQueries';
+import { GET_PROJECTS } from '../queries/projectQueries';
 
 export default function ClientRow({ client }) {
   const [deleteClient] = useMutation(DELETE_CLIENT, {
     variables: { id: client.id },
-    refetchQueries: [{ query: GET_CLIENTS }], //refetch data from DB
-    // update(cache, { data: { deleteClient } }) { //fetch data from client after updating it
-    //   const { clients } = cache.readQuery({ query: GET_CLIENTS });
-    //   cache.writeQuery({
-    //     query: GET_CLIENTS,
-    //     data: {
-    //       clients: clients.filter((client) => client.id !== deleteClient.id),
-    //     },
-    //   });
-    // },
+    refetchQueries: [{ query: GET_CLIENTS }, { query: GET_PROJECTS }]
   })
 
 
